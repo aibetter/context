62d1791796eaee84d3274385cb8d3e6bc72b73c4
litingyes <luz.liting@gmail.com>
2025-08-05 08:22:31 +0800

    style(theme): enable view transition

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,12 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
 import McpServers from './views/McpServers.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
     app.component(McpServers.name!, McpServers)
   },
